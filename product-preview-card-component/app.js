e15506a1ea590408206ec00ec505e3ffb90794b2
Smgy94 <Shanemcgeown@gmail.com>
2022-08-10 17:27:26 +0100

    Removed <p> element from button
    Removed <p> element from button to improve HTML Validations

--- product-preview-card-component/app.js
+++ product-preview-card-component/app.js
@@ -1,7 +1,5 @@
 const checkoutButton = document.getElementById("btn-id")
 const cartLogo = document.getElementById("cart-id")
-
-
 checkoutButton.onclick = function() {
   cartLogo.classList.toggle('rotate');
 }
