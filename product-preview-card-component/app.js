6261ecb87a0b3f47853da5a68a1323b0d571ae68
Smgy94 <Shanemcgeown@gmail.com>
2022-08-10 15:02:17 +0100

    Rotate cart icon on click.
    I've added an .onclick funtion to the checkout button that makes the shopping cart icon rotate.
    
    Also added a box-shadow to the card container.

---
+++ product-preview-card-component/app.js
@@ -0,0 +1,7 @@
+const checkoutButton = document.getElementById("btn-id")
+const cartLogo = document.getElementById("cart-id")
+
+
+checkoutButton.onclick = function() {
+  cartLogo.classList.toggle('rotate');
+}
