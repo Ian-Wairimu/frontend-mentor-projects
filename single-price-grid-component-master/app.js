212bc80309bbf0123c9a70438610796b375cd7df
Smgy94 <Shanemcgeown@gmail.com>
2022-08-15 22:33:31 +0100

    added app.js

---
+++ single-price-grid-component-master/app.js
@@ -0,0 +1,11 @@
+var item = document.getElementById("btn-text");
+item.addEventListener("mouseover", func, false);
+item.addEventListener("mouseout", func1, false);
+
+function func() {
+    item.innerHTML = "Follow Me"
+}
+function func1() {
+    item.innerHTML = "Follow Me"
+}
+
